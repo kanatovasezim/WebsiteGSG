17439ef6fe35f39aff499c88654cf77a9d3fb81b
kanatovasezim <kanatova_s@auca.kg>
2020-08-14 22:38:21 +0600

    deleted

--- gsg/src/main/java/kg/itacademy/gsg/exceptions/DontBeHackerException.java
+++
@@ -1,9 +0,0 @@
-package kg.itacademy.gsg.exceptions;
-
-public class DontBeHackerException extends Exception {
-    private static final String message = "Don't be a hacker...";
-
-    public DontBeHackerException() {
-        super(message);
-    }
-}
--- gsg/src/main/java/kg/itacademy/gsg/exceptions/RecordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package kg.itacademy.gsg.exceptions;
-
-public class RecordNotFoundException extends RuntimeException {
-    public RecordNotFoundException(String message) {
-        super(message);
-    }
-}
